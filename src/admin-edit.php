6d430132396dec830dcfb40940c562fdef0cc5e1
calebSmithDS <109048857+calebSmithDS@users.noreply.github.com>
2024-10-16 12:42:32 +1030

    admin stuff

--- src/admin-edit.php
+++ src/admin-edit.php
@@ -37,11 +37,11 @@
     </header>
     <div class="grid-container">
         <div class="grid-item-header"></div>
-    <div class="grid-item1">
+    <div class="card">
         <div class="card-header1">
             <h3 class="card-header1-item1">Users</h3>
             <div class="spacer"></div>
-            <a href="add-user.php"><h3 class="add-user">+</h3></a>
+            <a href="admin-add-user.php"><img class="add-img" src="../public\static/images/icons/add-user.png"></a>
         </div>
         <table class="f2-table" id="DisplayTable">
             <thead>
@@ -55,21 +55,21 @@
                 </tr>
             </thead>
             <tbody>
-                <?php
-                    require_once "dbconn.inc.php"; 
-                    $sql = "SELECT * FROM users;";
+            <?php
+                    require_once "./api/dbconn.inc.php"; 
+                    $sql = "SELECT * FROM employees;";
 
                     if($result = mysqli_query($conn, $sql)) {
                         if(mysqli_num_rows($result) >= 1) {
                             while($row = mysqli_fetch_assoc($result)) {
                                 echo 
-                                '<tr>
-                                    <td>' . $row['id'] . '</td>
-                                    <td>' . $row['fName'] . '</td>
-                                    <td>' . $row['lName'] . '</td>
+                                '<tr class="row-hover">
+                                    <td>' . $row['employee_id'] . '</td>
+                                    <td>' . $row['f_name'] . '</td>
+                                    <td>' . $row['l_name'] . '</td>
                                     <td>' . $row['role'] . '</td>
-                                    <td> <a class="details-btn" href=admin-details.php?id=' . $row['id'] . '>details</a> </td>
-                                    <td> <a class="delete-btn" href=delete.php?id=' . $row['id'] . '>delete</a> </td>'
+                                    <td> <a class="details-btn" href=admin-view-details.php?id=' . $row['employee_id'] . '>details</a> </td>
+                                    <td> <a class="delete-btn" href=api/admin/admin-delete.php?id=' . $row['employee_id'] . '><img class="delete-img" src="../public\static/images/icons/delete-user.png"></a> </td>'
                                     ;
                             }
                         }
@@ -82,41 +82,43 @@
 
     </div>
 <!-- grid-item2 - I want to act as a dynamic card interface, that allows me to add and edit users -->
-    <div class="grid-item2">
+    <div class="card">
         <div class="card-header1">
         <h3 class="card-header1-item1">User Details</h3> 
         
         <?php 
-        require_once "dbconn.inc.php";
+        require_once "./api/dbconn.inc.php";
         $id = $_GET["id"];
         echo
-        '<div><a class="edit-btn" href=edit.php?id="'. $id . '">edit</a></div>';
+        '<div><a class="edit-btn" href=admin-edit.php?id='. $id . '><img class="add-img" src="../public\static/images/icons/edit-user.png"></a></div>';
         ?>
         
     
     </div>
     <div class="grid-item2-content">
+        <p>Input values into the fields that you would like to edit</p>
         <?php 
-        require_once "dbconn.inc.php";
+        require_once "./api/dbconn.inc.php";
         $id = $_GET["id"];
-        $sql = "SELECT * FROM users WHERE id = $id;";
+        $sql = "SELECT * FROM employees WHERE employee_id = $id;";
 
         if($result = mysqli_query($conn, $sql)) {
             $row = mysqli_fetch_assoc($result);
             echo 
-            '<form action="includes/admin-edit.php" method="post">
-            <input type="hidden" name="id" value' . $row["id"] . '>
+            '<form action="api/admin/admin-edit-handler.php" method="post">
+            <input type="hidden" name="id" value="' . $row["employee_id"] . '">
             <div class="input-box">
                 <span class="details">First Name</span> 
-                <input type="text" name="fname" placeholder="' . $row["fName"] . '" required>
+                <input type="text" name="fname">
             </div>
             <div class="input-box">
                 <span class="details">Last Name</span> 
-                <input type="text" name="lname" placeholder="' . $row["lName"] . '" required>
+                <input type="text" name="lname">
             </div>
             <div class="input-box">
                 <span class="details">Role</span> 
-                <select name="role" id="role" placeholder="' . $row["role"] . '">
+                <select name="role" id="role">
+                    <option value="unchanged">unchanged</option>
                     <option value="Admin">Admin</option>
                     <option value="Factory Manager">Factory Manager</option>
                     <option value="Factory Worker">Factory Worker</option>
@@ -125,20 +127,16 @@
             </div>
             <div class="input-box">
                 <span class="details">Phone Number</span> 
-                <input type="text" name="phno" placeholder="' . $row["phoneNumber"] . '" required>
+                <input type="text" name="phno">
             </div>
-            <div class="gender-details">
-                <span class="gender-title">Gender</span>
-                <select name="gender" id="gender" placeholder="' . $row["gender"] . '">
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Other">Other</option>
-                </select>
+            <div class="input-box">
+                <span class="email-title">Email</span>
+                <input type="text" name="email">
             </div>
 
         
            <div class="button">
-                <input type="submit" name="submit" value="Update User">
+                <input class="submit-btn" type="submit" name="submit" value="Update User">
            </div>
         </form>'
             
--- src/api/admin/admin-edit-handler.php
+++ src/api/admin/admin-edit-handler.php
@@ -7,20 +7,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // ensures you access this page the 
     $lname = $_POST["lname"];
     $role = $_POST["role"];
     $phno = $_POST["phno"];
-    $gender = $_POST["gender"];
+    $email = $_POST["email"];
 
-    $id = $_POST["id"];
+    $id = (int)$_POST["id"]; //new this was the issue araghh
 
-    require_once "../dbconn.inc.php";
    
 
-    $sql = "UPDATE users SET fname = ?, lname = ?, role = ?, phoneNumber = ?, gender = ?
-    WHERE id = ?;";
+    require_once __DIR__ . "/../dbconn.inc.php";
+        // $id = $_GET["id"];
+        $sql = "SELECT * FROM employees WHERE employee_id = $id;";
+        
+        $result1 = mysqli_query($conn, $sql);
+        echo "test";
+        if($result = mysqli_query($conn, $sql)) {
+            echo "test";
+            $row = mysqli_fetch_assoc($result);
+            if ($fname == "") {
+                $fname = $row["f_name"];
+            }
+            if ($lname == "") {
+                $lname = $row["l_name"];
+            }
+            if ($role == "unchanged") {
+                $role = $row["role"];
+            }
+            if ($phno == "") {
+                $phno = $row["phoneNo"];
+            }
+            if ($email == "") {
+                $email = $row["email"];
+            }
+        }
+     
+
+    require_once __DIR__ . "/../dbconn.inc.php";
+   
+
+    $sql = "UPDATE employees SET f_name = ?, l_name = ?, role = ?, phoneNo = ?, email = ?
+    WHERE employee_id = ?;";
 
 $statement = mysqli_stmt_init($conn); // will intialise new SQL statment using stablised db connection stored in $conn
 mysqli_stmt_prepare($statement, $sql);
 
-mysqli_stmt_bind_param($statement, "ssssss", $fname, $lname, $role, $phno, $gender, $id);
+mysqli_stmt_bind_param($statement, "ssssss", $fname, $lname, $role, $phno, $email, $id);
 // set id to auto increment
 
 
@@ -30,14 +59,27 @@ mysqli_stmt_bind_param($statement, "ssssss", $fname, $lname, $role, $phno, $gend
 // mysqli_stmt_bind_param($statement, ':d', $phno); 
 // mysqli_stmt_bind_param($statement, ':e', $gender); 
 
+
+// $sql1 = "SELECT MIN(employee_id) AS id FROM employees;";
+        
+//         if($result = mysqli_query($conn, $sql1)) {
+            
+//             $row = mysqli_fetch_assoc($result);
+        
+//         }
+
+
+
+
 if (mysqli_stmt_execute($statement)) {
-    header("location: ../adminIndex.php"); }
+    header("Location: ../../admin-view-details.php?id=" . $id); }
     else {        
         echo mysqli_error($conn);
 }
 mysqli_close($conn);
 
-    header("Location: ../adminIndex.php"); // sends page back to index page
+    header("Location: ../../admin-view-details.php?id=". $id); // sends page back to index page
 } else {
-    header("Location: ../adminIndex.php"); // fail safe for security 
-}
+    header("Location: ../../admin-view-details.php?id=". $id); // fail safe for security 
+} 
+// save me do more code just write a handler that puts you on the view details page with the above logic
