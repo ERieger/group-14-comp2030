f1d7f690f9622f053d6fcadb14ead152beaccf8d
mahek491 <bhat0280@flinders.edu.au>
2024-10-18 08:09:15 +1030

    njvb

--- src/factoryworkerdashboard.php
+++ src/factoryworkerdashboard.php
@@ -178,29 +178,3 @@ $result_machines = mysqli_query($conn, $sql_machines);
         </div>
     </form>
 </div>
-
-<?php
-require_once '../src/api/dbconn.inc.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['machineName'])) {
-    $machineName = mysqli_real_escape_string($conn, $_POST['machineName']);
-
-    // Update the 'jobs' table to assign the machine to all employees
-    $sql_update = "UPDATE jobs j
-                   JOIN machines m ON j.machine_id = m.machine_id
-                   SET j.machine_id = (SELECT machine_id FROM machines WHERE machine_name = '$machineName')
-                   WHERE m.machine_name != '$machineName'";
-
-    if (mysqli_query($conn, $sql_update)) {
-        echo "Machine assigned successfully to all employees.";
-    } else {
-        echo "Error: " . mysqli_error($conn);
-    }
-
-    mysqli_close($conn);
-
-    // Redirect back to the main page or display a message
-    header("Location: your_main_page.php"); // Replace 'your_main_page.php' with the appropriate page
-    exit();
-}
-?>
