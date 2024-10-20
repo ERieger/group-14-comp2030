ee0b7d12b1e2075b96550cba4bd0128daead506d
mahek491 <bhat0280@flinders.edu.au>
2024-10-20 15:56:40 +1030

    save test

--- src/add_new_employee-form.php
+++ src/add_new_employee-form.php
@@ -4,11 +4,10 @@ require_once '../src/api/dbconn.inc.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $phone_no = $_POST['phone_no']; // Get the phone number from the form
+    $phone_no = $_POST['phone_no']; 
     $machine_id = $_POST['machine_name'];
     $role = 'Production Operator';
 
-    // Insert the new employee into the employees table
     $sql_employee = "INSERT INTO employees (f_name, l_name, phoneNo, role) VALUES (?, ?, ?, ?)";
     $stmt_employee = mysqli_prepare($conn, $sql_employee);
     mysqli_stmt_bind_param($stmt_employee, 'ssss', $first_name, $last_name, $phone_no, $role);
@@ -16,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (mysqli_stmt_execute($stmt_employee)) {
         $employee_id = mysqli_insert_id($conn);
         
-        // Assign the machine to the new employee
+ 
         $sql_job = "INSERT INTO jobs (employee_id, machine_id) VALUES (?, ?)";
         $stmt_job = mysqli_prepare($conn, $sql_job);
         mysqli_stmt_bind_param($stmt_job, 'ii', $employee_id, $machine_id);
