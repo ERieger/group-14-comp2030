9ae5dca07cad383b26694c67ec0f5b85a1aed652
calebSmithDS <109048857+calebSmithDS@users.noreply.github.com>
2024-10-21 09:22:17 +1030

    fixed headers

--- src/admin-edit.php
+++ src/admin-edit.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin-edit-user</title>
     <link rel="stylesheet" href="../public/static/css/normalize.css">
     <link rel="stylesheet" href="../public/static/css/colours.css">
     <link rel="stylesheet" href="../public/static/css/utility.css">
